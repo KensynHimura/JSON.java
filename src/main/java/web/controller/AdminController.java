c6b0d85f62776047fbae45831cb730ed9c254485
Irina Slobodianiuk <slobodyanyukia@gmail.com>
2021-12-22 18:52:40 +0300

    commit

--- src/main/java/web/controller/AdminController.java
+++ src/main/java/web/controller/AdminController.java
@@ -1,8 +1,5 @@
 package web.controller;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -10,8 +7,6 @@ import web.model.Role;
 import web.model.User;
 import web.service.UserService;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.security.Principal;
 import java.util.HashSet;
 import java.util.Set;
